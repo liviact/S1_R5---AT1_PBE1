8eb527dab282a587bdd9f48fbc3cfdf98825cb45
unknown <lilicaetano19@gmail.com>
2025-09-09 15:01:20 -0300

    comentado ex1

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 app.post('/informacoes', (req, res) => {
     try {
+        //recebendo as informações do usuário através do body
        const {informacoes: {nome, idade, timeFavorito}} = req.body;
        console.log(nome, idade, timeFavorito);
+       //respondendo com uma mensagem de acordo com os dados do usuário
     res.status(201).json({message: `olá, ${nome}, você tem ${idade} anos e seu time do coração 
         é ${timeFavorito}. ótima escolha!`});
     } catch (error) {
