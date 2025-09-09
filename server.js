676e8fe590eb3e9b9182f744c1fbd6c3bf0a51f4
unknown <lilicaetano19@gmail.com>
2025-09-09 14:59:14 -0300

    ex2 comentado

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ app.use(express.json());
 
 //solicitando os números e tratando caso não sejam válidos
 app.post('/soma', (req, res) => {
-    try {
+    try { //desestruturando de acordo com o formato recebido do cliente
        const {soma: {num1, num2, num3}} = req.body;
        console.log(num1, num2, num3);
+       //tratando caso sejam inválidos os valores
        if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
             return res.status(404).send("valor inválido!");
-        } 
+        } //caso estejam válidos, ele faz a soma e devolve o resultado
     let resultado = parseFloat(num1)+parseFloat(num2)+parseFloat(num3)
     res.status(201).json({message: `olá, a soma dos números é  ${resultado}`});
     } catch (error) {
