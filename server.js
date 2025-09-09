3a1dd387e49d6bc1a60f414e21573b9496b1d2ce
unknown <lilicaetano19@gmail.com>
2025-09-09 14:56:39 -0300

    ex3 comentado

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(express.json());
 app.post('/login', (req, res) => {
     try {
         const { login: { usuario, senha } } = req.body;
-    
+    //ignorando se a letra é maiúscula ou minúscula e estipilando um valor estático para senha e usuário
         if (usuario.toLowerCase() === "livia.caetano" && Number(senha) === 1234) {
+            //caso o valor esteja correto, ele devolve uma mensagem avisando. caso não esteja, ele manda uma 'error message'""
             res.status(201).json({ message: `usuário e senha corretos!` });
         } else { 
             (res.status(401).json ({errorMessage: `usuário ou senha inválidos` })); 
